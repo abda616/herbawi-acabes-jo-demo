600ead8cbdfe3014106a47e26cd73318929dcd81
herbawi-esense <aherbawi@esensesoftware.com>
2025-09-13 16:28:50 +0300

    11th-push

--- src/main.py
+++ src/main.py
@@ -5,15 +5,15 @@ import os
 env_host = os.getenv('ENV_VAR_REDIS', 'localhost')
 env_key = os.getenv('ENV_VAR_KEY', 'hello')
 env_value = os.getenv('ENV_VAR_VALUE', 'world!')
-env_port =  int(os.getenv('ENV_VAR_PORT', "6379"))
+env_port =  os.getenv('ENV_VAR_PORT', "6379")
 
 
 # Connect to Redis
-redis_client = redis.StrictRedis(host=env_host, port=env_port, db=0)
+redis_client = redis.StrictRedis(host=env_host, port=int(env_port), db=0)
 check_key = redis_client.get(env_key)
 # Set a default value if not already set
 if not check_key:
     print(f"missing key, setting default value. '{env_key}': {env_value}")
     redis_client.set(env_key, env_value)
 else:
-    print(f"Value for '{env_key}': {redis_client.get(env_key)}")
+    print(f"Hit, Value for '{env_key}': {redis_client.get(env_key)}")
